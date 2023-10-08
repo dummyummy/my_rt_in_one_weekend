bef428d8a92b4e23a2b2f07557640b39a022710d
pacman000 <3384447470@qq.com>
2023-10-08 18:51:08 +0800

    Head for next week.

--- main.cpp
+++ main.cpp
@@ -351,9 +351,94 @@ void cornell_smoke() {
     image.save_png("cornell_smoke.png");
 }
 
+void final_scene()
+{
+    hittable_list boxes1;
+    auto ground = make_shared<lambertian>(color(0.48, 0.83, 0.53));
+
+    int boxes_per_side = 20;
+    for (int i = 0; i < boxes_per_side; i++)
+    {
+        for (int j = 0; j < boxes_per_side; j++)
+        {
+            auto w = 100.0;
+            auto x0 = -1000.0 + i * w;
+            auto z0 = -1000.0 + j * w;
+            auto y0 = 0.0;
+            auto x1 = x0 + w;
+            auto y1 = random_double(1, 101);
+            auto z1 = z0 + w;
+
+            boxes1.add(box(point3(x0, y0, z0), point3(x1, y1, z1), ground));
+        }
+    }
+
+    hittable_list world;
+
+    world.add(make_shared<bvh_node>(boxes1));
+
+    auto light = make_shared<diffuse_light>(color(7, 7, 7));
+    world.add(make_shared<quad>(point3(123, 554, 147), vec3(300, 0, 0), vec3(0, 0, 265), light));
+
+    auto center1 = point3(400, 400, 200);
+    auto center2 = center1 + vec3(30, 0, 0);
+    auto sphere_material = make_shared<lambertian>(color(0.7, 0.3, 0.1));
+    world.add(make_shared<sphere>(center1, center2, 50, sphere_material));
+
+    world.add(make_shared<sphere>(point3(260, 150, 45), 50, make_shared<dielectric>(1.5)));
+    world.add(make_shared<sphere>(
+        point3(0, 150, 145), 50, make_shared<metal>(color(0.8, 0.8, 0.9), 1.0)));
+
+    auto boundary = make_shared<sphere>(point3(360, 150, 145), 70, make_shared<dielectric>(1.5));
+    world.add(boundary);
+    auto inner_boundary = make_shared<sphere>(point3(360, 150, 145), 70, make_shared<dielectric>(1.5));
+    world.add(make_shared<constant_medium>(inner_boundary, 0.01, color(0.2, 0.4, 0.8)));
+    boundary = make_shared<sphere>(point3(0, 0, 0), 5000, make_shared<lambertian>(color(0, 0, 0)));
+    world.add(make_shared<constant_medium>(boundary, .0001, color(1, 1, 1)));
+
+    auto emat = make_shared<lambertian>(make_shared<image_texture>("earthmap.png"));
+    world.add(make_shared<sphere>(point3(400, 200, 400), 100, emat));
+    auto pertext = make_shared<noise_texture>(0.1, 10.0, 0);
+    world.add(make_shared<sphere>(point3(220, 280, 300), 80, make_shared<lambertian>(pertext)));
+
+    hittable_list boxes2;
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    int ns = 1000;
+    for (int j = 0; j < ns; j++)
+    {
+        boxes2.add(make_shared<sphere>(point3::random(0, 165), 10, white));
+    }
+
+    world.add(make_shared<translate>(
+        make_shared<rotate_y>(
+            make_shared<bvh_node>(boxes2), 15),
+        vec3(-100, 270, 395)));
+
+    camera cam;
+
+    cam.aspect_ratio = 1.0;
+    cam.image_width = 800;
+    cam.samples_per_row = 4;
+    cam.samples_per_subpixel = 4;
+    cam.max_depth = 50;
+    cam.background = color(0, 0, 0);
+
+    cam.vfov = 40;
+    cam.lookfrom = point3(478, 278, -600);
+    cam.lookat = point3(278, 278, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+    cam.initialize();
+
+    CImg<unsigned char> image(cam.image_width, cam.image_height, 1, 3);
+    cam.render(image, world);
+    image.save_png("final_scene.png");
+}
+
 int main()
 {
-    int scene_id = 8;
+    int scene_id = 9;
     switch (scene_id)
     {
     case 1:
@@ -379,6 +464,9 @@ int main()
         break;
     case 8:
         cornell_smoke();
+        break;
+    case 9:
+        final_scene();
         break;
     default:
         break;
